2e0ce33b8790119bb5a80c5e7d8ce4e66e0728b4
prachi upreti <prachiupreti2108@gmail.com>
2026-07-06 07:36:05 +0530

    Time: 0 ms (100%), Space: 15.3 MB (20.13%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+
+        int n = intervals.size();
+
+        sort(intervals.begin(), intervals.end(),
+            [](vector<int>& a, vector<int>& b) {
+                if (a[0] == b[0])
+                    return a[1] > b[1];
+                return a[0] < b[0];
+            });
+
+        int ans = n;
+
+        int maxEnd = intervals[0][1];
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][1] <= maxEnd) {
+                ans--;              
+            } else {
+                maxEnd = intervals[i][1];
+            }
+        }
+
+        return ans;
+    }
+};
