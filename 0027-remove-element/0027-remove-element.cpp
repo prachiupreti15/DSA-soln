f5b9e2526ea5e024150d806ce0ad8052bd5cb61c
prachi upreti <prachiupreti2108@gmail.com>
2026-07-27 07:32:39 +0530

    Time: 0 ms (100%), Space: 11.9 MB (12.9%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int n = nums.size();
+        int pointer = -1;
+        int shift = n-1 ;
+
+        if (n==0) return 0;
+        if (n==1 && nums[0] != val) return 1;
+
+        int i = 0 ;
+        while ( i < n ){
+            if (nums[i] == val && i != pointer){
+                swap(nums[i] , nums[shift]);
+                pointer = shift;
+                shift--; 
+                i--;            
+            }
+            i++ ;
+            if (i == pointer) break;
+        }
+        if (pointer == -1) return n;
+        return pointer;
+    }
+};
