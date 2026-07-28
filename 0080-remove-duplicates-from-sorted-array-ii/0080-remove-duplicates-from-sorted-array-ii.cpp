98f12f6403f73dc5db97548ec264d710e96ef408
prachi upreti <prachiupreti2108@gmail.com>
2026-07-28 17:13:15 +0530

    Time: 9 ms (49.95%), Space: 19.6 MB (61.01%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         if (n <= 2) return n;
 
-        int k = 2; // first two elements are always allowed
+        int k = 2; 
 
         for (int i = 2; i < n; i++) {
             if (nums[i] != nums[k - 2]) {
